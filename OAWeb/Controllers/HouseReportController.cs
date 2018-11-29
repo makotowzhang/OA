0c75aa9e6d6ecf099ce888f492809319e8d1771d
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2018-11-29 18:47:11 +0800

    dev

--- OAWeb/Controllers/HouseReportController.cs
+++ OAWeb/Controllers/HouseReportController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Model.RMModel;
+using Business.RMBusiness;
+using Model.SystemModel;
 
 namespace OAWeb.Controllers
 {
     public class HouseReportController : BaseController
     {
+        HouseReportBusiness service = new HouseReportBusiness();
         // GET: HouseReport
         public ActionResult Index()
         {
@@ -20,9 +23,21 @@ namespace OAWeb.Controllers
             return View();
         }
 
+        public ActionResult GetUserCreateReport()
+        {
+            return Json(service.GetOrCreateModel(CurrentUser.Id));
+        }
+
         public ActionResult SaveReport(HouseReportModel model)
         {
-            return Content("");
+            model.UpdateUser = CurrentUser.Id;
+            return Json(new JsonMessage(service.SaveReport(model)));
+        }
+
+        public ActionResult SubmitReport(HouseReportModel model)
+        {
+            model.UpdateUser = CurrentUser.Id;
+            return Json(new JsonMessage(service.SaveReport(model,true)));
         }
     }
 }
--- OAWeb/Controllers/LeaseController.cs
+++ OAWeb/Controllers/LeaseController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Model.PMModel;
 using Business.PMBusiness;
 using Model.SystemModel;
+using Model.SystemModel;
 
 
 namespace OAWeb.Controllers
